ded1a463260d02ecf0e855e7a3adad3b5428d9f8
vanGeck <przemyslaw.hawel@gmail.com>
2023-07-31 00:42:12 +0200

    All stations operational

---
+++ Assets/_Project/_Life/DrillingStation/StorageStation.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Life
+{
+    public class StorageStation : MonoBehaviour
+    {
+        public static Action OnCollectionComplete = delegate { };
+        private Specimen[] _specimens = new Specimen[24];
+        [SerializeField] private SpecimenData[] _requiredSpecimens;
+
+        public void StoreItem(int idx, Specimen s)
+        {
+            Debug.Log("Storing item in slot " + idx + ".");
+            s.SpecimenProgress.Stored = true;
+            _specimens[idx] = s;
+            CheckVictoryCondition();
+        }
+
+        private void CheckVictoryCondition()
+        {
+            var required = _requiredSpecimens.ToList();
+            for (int i = 0; i < _specimens.Length; i++)
+            {
+                if (!_specimens[i]) continue;
+                if (required.Contains(_specimens[i].SpecimenData))
+                {
+                    required.Remove(_specimens[i].SpecimenData);
+                }
+            }
+            
+            if (required.Count == 0) OnCollectionComplete?.Invoke();
+        }
+    }
+}
